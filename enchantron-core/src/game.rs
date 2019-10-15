2d40398d34d372c2e9e8a01f9b5401b53dfc3849
Kevin Guthrie <kevin.guthrie@gmail.com>
2019-10-15 15:15:09 -0400

    More work on perlin noise

--- enchantron-core/src/game.rs
+++ enchantron-core/src/game.rs
@@ -1,7 +1,7 @@
 pub use self::terrain_provider::TerrainProvider;
 pub use self::terrain_type::TerrainType;
 
-pub use self::perlin_terrain_1;
+pub use self::perlin_terrain_1::PerlinTerrain1;
 
 mod terrain_provider;
 mod terrain_type;
--- enchantron-core/src/game/perlin_terrain_1.rs
+++ enchantron-core/src/game/perlin_terrain_1.rs
@@ -1,16 +1,12 @@
-
 use super::{TerrainProvider, TerrainType};
-use crate::model::{IPoint};
+use crate::model::IPoint;
 use fnv::FnvHasher;
 use std::hash::Hasher;
 
-pub struct PerlineTerrain1 {
+pub struct PerlineTerrain1 {}
 
-}
-
-impl TerrainProvider for PerlineTerrain1 {
-
-    fn get_for(&self, position: &IPoint) -> TerrainType {
+impl PerlineTerrain1 {
+    fn perlin_gradient(&self, octave: u32, position: &IPoint) -> (f64, f64) {
         let mut hasher = FnvHasher::default();
 
         hasher.write_i64(position.x);
@@ -18,8 +14,16 @@ impl TerrainProvider for PerlineTerrain1 {
 
         let hash = hasher.finish();
 
+        // Take the right of the hash as the dx and the left as the dy
+        let dx = ((hash as i32) as f64) / std::i32::MIN as f64;
+        let dy = ((hash as i32 >> 32) as f64) / std::i32::MIN as f64;
 
-
+        (dx, dy)
     }
 
+    fn lin_interp() -> f64 {}
+}
+
+impl TerrainProvider for PerlineTerrain1 {
+    fn get_for(&self, position: &IPoint) -> TerrainType {}
 }

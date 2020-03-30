b9b08d7ce172c1cbac76b9010ab52787c8a6a383
Kevin Guthrie <kevin.guthrie@gmail.com>
2020-03-30 08:23:09 -0400

    Starting to work on what is effectively an ecs

--- enchantron-core/src/game.rs
+++ enchantron-core/src/game.rs
@@ -1,12 +1,16 @@
+pub use self::player::Player;
 pub use self::terrain_provider::TerrainProvider;
 pub use self::terrain_type::TerrainType;
+pub use self::world_entity::WorldEntity;
 
 pub use self::perlin_terrain_1::PerlinTerrain1;
 
 pub mod constants;
 
+mod player;
 mod terrain_provider;
 mod terrain_type;
+mod world_entity;
 
 mod perlin_terrain_1;
 
---
+++ enchantron-core/src/game/world_entity.rs
@@ -0,0 +1,5 @@
+use super::Player;
+
+pub enum WorldEntity {
+    Player(Player),
+}
